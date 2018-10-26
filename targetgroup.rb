fdab850eb68f95c570332d2494b78c59c507d535
Llivingon <llivingon@yahoo.com>
2018-10-26 08:29:10 +0530

    final

--- targetgroup.rb
+++ targetgroup.rb
@@ -1,77 +1,46 @@
 module Serverspec
   module Type
     module AWS
-      # The ElasticLoadBalancing module contains the ElasticLoadBalancing API
+      # The ElasticLoadBalancing module contains the ElasticLoadBalancingV2 API
       # resources
       module ElasticLoadBalancingV2
-        # The LoadBalancer class exposes the ElasticLoadBalancing::LoadBalancer
+        # The LoadBalancer class exposes the ElasticLoadBalancing::LoadBalancerTGHealth
         # resources
         class LoadBalancerRule < Base
-          # AWS SDK for Ruby v2 Aws::ElasticLoadBalancing::LoadBalancerRule wrapper
+          # AWS SDK for Ruby v2 Aws::ElasticLoadBalancing::LoadBalancerTGHealth wrapper
           # for initializing a LoadBalancerRule resource
           # @param alb_name [String] The arn of the Rule
           # rubocop:disable LineLength
-          # @param instance [Class] Aws::ElasticLoadBalancing::LoadBalancerRule instance
+          # @param instance [Class] Aws::ElasticLoadBalancing::LoadBalancerTGHealth instance
           # rubocop:enable LineLength
           # @raise [RuntimeError] if albs.nil?
           # @raise [RuntimeError] if albs.length == 0
           # @raise [RuntimeError] if albs.length > 1
-          def initialize(rule_arn, instance = nil)
+          def initialize(target_group_arn, instance = nil)
             check_init_arg(
-              'rule_arn',
-              'ElasticLoadBalancingV2::LoadBalancerRule',
-              alb_arn
+              'target_group_arn',
+              'ElasticLoadBalancingV2::LoadBalancerTGHealth',
+              target_group_arn
             )
-            @alb_arn = alb_arn
+            @target_group_arn = target_group_arn
             get_instance instance
-            get_rule rule_arn
+            get_target_group_health target_group_arn
           end
 
-          # Indicates whether rule is_default
-          # @return [Boolean]
-          def is_default
-            @rule_details.is_default == true
-          end
-          
-          # Information about the priority on the rule
-          # @return [String]
-          def priority
-            @rule_details.priority
-          end
-          
-          # Information about the action type on the rule
-          # @return [String]
-          def action_type
-            @rule_details.actions[0].type
-          end          
-          
-          # Information about the target group arn on the rule
-          # @return [String]
-          def targetgroup_arn
-            @rule_details.actions[0].target_group_arn
-          end          
-          
-          # Information about the conditions on the rule
-          # @return [String]
-          def condition_field
-            @rule_details.conditions[0].field
-          end
-          
-          # Information about the condition values on the rule
+          # Indicates whether target_health is_healthy
           # @return [Array]
-          def condition_values
-            @rule_details.conditions[0].values
-          end
-          
+          def target_group_details
+            @target_group_details
+          end      
+
           private
 
           # @private
-          def get_rule(rule_arn)
-            rule_details = @aws.describe_rules(
-              listener_arn: [rule_arn]
-            ).rules
-            check_length 'rule details', rule_details
-            @rule_details = rule_details[0]
+          def describe_target_health(target_group_arn)
+            target_group_details = @aws.describe_target_health(
+              target_group_arn: target_group_arn
+            ).target_health_descriptions
+            @target_group_details = target_health_descriptions
           end
           
           # @private
